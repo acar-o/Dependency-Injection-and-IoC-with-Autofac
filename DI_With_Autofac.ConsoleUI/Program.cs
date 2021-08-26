6bbb6868dc61ade3209ff5f685419e224c0884b2
oacar <o.acar@propertyexpert.de>
2021-08-26 15:25:15 +0200

    Add project files.

---
+++ DI_With_Autofac.ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BusinessLogic businessLogic = new();
+            //businessLogic.ProcessData();
+
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
